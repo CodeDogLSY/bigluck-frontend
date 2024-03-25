25d63172376b61bb761b28aefc9e29cb7599fec0
CodeDogLSY <liushiyang2016@gmail.com>
2024-03-25 17:28:17 +0800

    排版处理

--- src/pages/schedulesManage/index.vue
+++ src/pages/schedulesManage/index.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="page">
-    <nut-calendar-card v-model="value" @change="onChange"></nut-calendar-card>
+    <nut-calendar-card v-model="employees.selectDay" @change="onChange"></nut-calendar-card>
     <view class="item-list">
 
       <view class="item-item top">
@@ -8,9 +8,10 @@
         <text class="item-description">值班人</text>
         <text class="item-history">联系方式</text>
       </view>
-      <view class="item-item" v-for="item in employees.list" :key="item.itemId">
+      <view class="item-item" v-for="item in employees.list" :key="item.itemId"
+        :class="{ select: isSelect(item.schedule_date) }" @click="setDate(item.schedule_date)">
         <text class="item-name">{{ item.schedule_date }}</text>
-        <text class="item-description">{{ item.employee_id }}</text>
+        <text class="item-description">{{ item.employee_name }}</text>
         <text class="item-history">123456</text>
       </view>
     </view>
@@ -19,17 +20,33 @@
 
 <script setup>
 import './index.scss'
-import { reactive,ref } from 'vue'
+import dayjs from 'dayjs'
+import { reactive, ref } from 'vue'
 import Taro from '@tarojs/taro'
 import { getSchedules } from '../../api/api'
 const employees = reactive({
-  list: []
+  list: [],
+  selectDay: new Date()
 })
 
-const value = ref(new Date('2023-01-01'));
 const onChange = (val) => {
-  console.log(val);
-};
+  console.log(val)
+  const formattedDate = dayjs(val).format('YYYY-MM-DD')
+  console.log(formattedDate)
+}
+
+const isSelect = (data) => {
+  const formattedDate = dayjs(employees.selectDay).format('YYYY-MM-DD')
+  if (data == formattedDate) {
+    console.log(data)
+    console.log(formattedDate)
+  }
+  return data == formattedDate
+}
+
+const setDate = (data) => {
+  employees.selectDay = new Date(data)
+}
 
 Taro.showLoading()
 getSchedules().then(res => {
